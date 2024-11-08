8e442357d7ac1d4fa5029419dfa46326fd56db8b
LeireMartin <leiremartinlusarreta@gmail.com>
2024-11-08 11:15:04 +0100

    Rellenar datos

--- 02_EstudioArraysCadenas/02_EstudiArraysyCadenas/src/estudioArrays/EjercicioNotas.java
+++ 02_EstudioArraysCadenas/02_EstudiArraysyCadenas/src/estudioArrays/EjercicioNotas.java
@@ -3,6 +3,13 @@ package estudioArrays;
 public class EjercicioNotas {
     public EjercicioNotas(){
 
+        String[] asignaturas={"Programación", "BBDD","Lenguaje de Marcas","Fol","Sistemas"};
+        String[] nombres={"Ana","Juan","Pepe","Leire","Alex","Ivan","Pepa","Luis","Chus","Tere"};
+        int [][]notas={{},
+                       {},
+                       {},
+                       {},
+                       {}};
         
     }
 
